1ece56dc959fd3a3ba1c6c7c8771778746816e3e
Andredsonn <andreaesilva6@gmail.com>
2025-08-13 22:33:00 -0300

    Aula 03Colecoes

---
+++ Models/Funcionario.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aula03Colecoes.Models.Enuns;
+
+//Um comentário qualquer pra deixar no git
+
+namespace Aula03Colecoes.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public decimal Salario { get; set; }
+        public TipoFuncionarioEnum TipoFuncionario { get; set; }
+
+        public void ReajustarSalario()
+        {
+            Salario = Salario + (Salario * 10/100);
+        }
+
+        public string ExibirPeriodoExperiencia()
+        {
+            string periodoExperiencia 
+                = string.Format("Período de Experiência: {0} até {1}", 
+                    DataAdmissao, DataAdmissao.AddMonths(3));
+
+            return periodoExperiencia;
+        }
+
+        public decimal CalcularDescontoVT(decimal percentual)
+        {
+            decimal desconto = this.Salario * percentual/100;
+            return desconto;
+        }        
+    }
+}
